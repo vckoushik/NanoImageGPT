64902ae9a674cb5b3e387822d3600193153834bf
Koushik Siva <koushiksiva9@gmail.com>
2024-03-28 20:35:18 -0500

    Remove GPU settings

--- data/imageprocess/prepare.py
+++ data/imageprocess/prepare.py
@@ -5,12 +5,6 @@ import imghdr
 import numpy as np
 from matplotlib import pyplot as plt
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus: 
-    tf.config.experimental.set_memory_growth(gpu, True)
-
-tf.config.list_physical_devices('GPU')
-
 data_dir = 'v_data'
 image_exts = ['jpeg','jpg', 'bmp', 'png']
 for image_class in os.listdir(data_dir): 
